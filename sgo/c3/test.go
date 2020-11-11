d7129ba9e76417cf6dff2702fbe0276483d9fdd4
u2i <pv3f3wii@s.okayama-u.ac.jp>
2020-11-11 21:00:34 +0900

    add c3

---
+++ sgo/c3/test.go
@@ -0,0 +1,35 @@
+package main
+
+// comments
+
+/* comments */
+
+/*
+    comments
+*/
+
+import (
+    "fmt"
+)
+
+func main() {
+
+    var i int // comments
+    i = 1
+    fmt.Println(i)
+
+    fmt.Println("Hello, World!")
+    fmt.Println("Hello, World!");
+
+    a := [3]string{
+        "Yamada Taro",
+        "Sato Hanako",
+        "Suzuki Kenji",
+    }
+    fmt.Println(a)
+
+    n := 1
+    fmt.Println(n)
+
+}
+
