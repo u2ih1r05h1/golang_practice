2601983e1eb815532541fae3060fdc7d9508cc64
u2i <pv3f3wii@s.okayama-u.ac.jp>
2020-11-12 19:04:29 +0900

    update c3

--- sgo/c3/test.go
+++ sgo/c3/test.go
@@ -31,5 +31,33 @@ func main() {
     n := 1
     fmt.Println(n)
 
+    n1 := 1 + 2 + 3 // n == 6
+
+    fmt.Println(n1)
+
+    fmt.Println("Hello, World!") // => "Hello, World!"
+
+    fmt.Println("Hello, Golang!")
+
+    fmt.Println("My", "name", "is", "Taro")
+
+    fmt.Printf("数値=%d\n", 5)
+
+    fmt.Printf("10進数=%d 2進数=%B 8進数=%o 16進数=%x\n", 17, 17, 17, 17)
+
+    fmt.Printf("%d年%d月%d日\n", 2015, 12)
+
+    fmt.Printf("%d年%d月%d日\n", 2015, 12, 25, 17)
+
+    fmt.Printf("数値=%v 文字列=%v 配列=%v\n", 5, "Golang", [...]int{1, 2, 3})
+
+    fmt.Printf("数値=%#v 文字列=%#v 配列=%#v\n", 5, "Golang", [...]int{1, 2, 3})
+
+    fmt.Printf("数値=%T 文字列=%T 配列=%T\n", 5, "Golang", [...]int{1, 2, 3})
+
+    print("Hello, World!")
+    println("Hello, World!")
+
+    println(1, 2, 3)
 }
 
